3fc02edf187f23cd5b2bc7d324c3d6533015dc65
Usman Sajjad <143373873+UsmanSajjad04-10@users.noreply.github.com>
2023-12-18 09:06:45 +0500

    Update email.php

--- email.php
+++ email.php
@@ -6,7 +6,9 @@ use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
 //Load Composer's autoloader
-require 'vendor/autoload.php';
+require 'PHPMailer/Exception.php';
+require 'PHPMailer/PHPMailer.php';
+require 'PHPMailer/SMTP.php';
 
 //Create an instance; passing `true` enables exceptions
 $mail = new PHPMailer(true);
